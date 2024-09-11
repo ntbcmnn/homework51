6213d4aa005d3734c645f842e2a7305f90ac1e04
Natalya Botsman <botsmannatashaa@gmail.com>
2024-09-11 15:59:31 +0600

    Created a function for rendering random numbers & button, that uses this function

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
 import './App.css'
+import {useState} from "react";
+import Number from "./components/Number/Number.tsx";
 
-const App = () => (
-    <>
-    <div className="hi">
-        hello
-    </div>
-    </>
-);
+const App = () => {
+    const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
+
+    const changeNumbers = () => {
+        const newNumbers: number[] = [];
+
+        while (newNumbers.length < numbers.length) {
+            const randomNum = Math.floor(Math.random() * (36 - 5 + 1)) + 5;
+            if (!newNumbers.includes(randomNum)) {
+                newNumbers.push(randomNum);
+            }
+        }
+
+       setNumbers(newNumbers.sort((a, b) => a - b));
+    };
+
+    return (
+        <>
+            <div className="container">
+                <button type={"button"} className={"changeBtn"} onClick={changeNumbers}>Click</button>
+
+                {numbers.map((num, index) => (
+                    <Number key={index} num={num}/>
+                ))}
+            </div>
+        </>
+    )
+};
 
 export default App
+
