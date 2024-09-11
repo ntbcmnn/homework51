2e2b1c82c940ab65f164d5793da2dc2f6e89a495
Natalya Botsman <botsmannatashaa@gmail.com>
2024-09-11 16:47:31 +0600

    Added typization for variables

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,28 @@ import Number from "./components/Number/Number.tsx";
 const App = () => {
     const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
 
-    const changeNumbers = () => {
+    const changeNumbers = (): void => {
         const newNumbers: number[] = [];
 
         while (newNumbers.length < numbers.length) {
-            const randomNum = Math.floor(Math.random() * (36 - 5 + 1)) + 5;
-            if (!newNumbers.includes(randomNum)) {
-                newNumbers.push(randomNum);
+            const randomNumber: number = Math.floor(Math.random() * (36 - 5 + 1)) + 5;
+            if (!newNumbers.includes(randomNumber)) {
+                newNumbers.push(randomNumber);
             }
         }
-
-       setNumbers(newNumbers.sort((a, b) => a - b));
+        setNumbers(newNumbers.sort((a: number, b: number) => a - b));
     };
 
     return (
         <>
             <div className="container">
-                <button type={"button"} className={"changeBtn"} onClick={changeNumbers}>Click</button>
+                <div className="numbers-container">
+                    {numbers.map((num: number, index: number) => (<Number key={index} num={num}/>))}
+                </div>
 
-                {numbers.map((num, index) => (
-                    <Number key={index} num={num}/>
-                ))}
+                <button type={"button"} className={"changeBtn"} onClick={changeNumbers}>
+                    <img src="https://www.svgrepo.com/show/18461/refresh.svg" alt="regenerate numbers"/>
+                </button>
             </div>
         </>
     )
